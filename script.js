3aea1bfd31c5d1e4fe0a3bd5ca3f9137d2963ff6
walexisojeda94-droid <w.alexisojeda94@gmail.com>
2026-08-06 15:47:03 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("frase").textContent = CONFIG.frase;
-
-  const insta = document.getElementById("btn-instagram");
-  insta.href = `https://instagram.com/${CONFIG.instagram}`;
-
-  const portfolio = document.getElementById("btn-portfolio");
-  portfolio.href = CONFIG.portfolio;
-});
